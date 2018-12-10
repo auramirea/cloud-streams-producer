1dec7870e8c90c652e057e85e89aba857c6d0e76
Aura Mirea <amirea@pivotal.io>
2018-12-10 11:11:20 +0100

    initial commit

---
+++ src/main/java/com/test/producer/Producer.java
@@ -0,0 +1,34 @@
+package com.test.producer;
+
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.Random;
+
+@Service
+@EnableBinding(Source.class)
+@EnableScheduling
+public class Producer {
+
+    public final Source source;
+
+    public Producer(Source source) {
+        this.source = source;
+    }
+
+    //@SendTo(Source.OUTPUT)
+    @Scheduled(fixedRate = 200)
+    public void produce() {
+        String letters = "abcdef";
+        String message = "message " + letters.charAt(new Random().nextInt(letters.length()));
+        System.out.println("Sending message " + message);
+        source.output().send(new GenericMessage<>(message));
+    }
+
+}
